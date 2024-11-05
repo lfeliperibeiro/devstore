54ad107fe46277e35edc837462ff721b3f3ea512
Felipe Ribeiro <piporibeiro@gmail.com>
2024-11-05 10:51:57 -0300

    style: adjust folder organization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+    <html className={`${inter.variable} antialiased`} lang="pt">
+      <body>{children}</body>
     </html>
   );
 }
